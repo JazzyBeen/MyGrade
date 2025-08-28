81a91af00d6469079f8f1e1f2bcc84adcc1ae9bd
Name Surname <your@email>
2025-08-28 23:39:29 +0300

    0.2 Commite

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         minSdk = 24
         targetSdk = 36
         versionCode = 1
-        versionName = "1.0"
+        versionName = "0.0.1 - alpha"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
@@ -36,6 +36,16 @@ dependencies {
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
+    implementation("com.google.api-client:google-api-client-android:1.33.0")
+    implementation("com.google.apis:google-api-services-sheets:v4-rev20211026-1.33.0")
+    implementation("com.google.http-client:google-http-client-android:1.39.2")
+    implementation("com.google.http-client:google-http-client-gson:1.39.2")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:okhttp:4.9.0")
+    implementation ("androidx.work:work-runtime-ktx:2.8.1")
+    implementation("com.google.code.gson:gson:2.10.1")
+    implementation("com.google.android.material:material:1.10.0")
     implementation(libs.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
--- app/src/main/java/com/android/mygrade/MainActivity.java
+++ app/src/main/java/com/android/mygrade/MainActivity.java
@@ -1,20 +1,46 @@
 package com.android.mygrade;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-
 public class MainActivity extends AppCompatActivity {
+
+    private int counter = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        ImageButton buttonPlus = findViewById(R.id.button_plus);
+        ImageButton buttonHistory = findViewById(R.id.button_history);
+        LinearLayout container = findViewById(R.id.container);
+
+        buttonPlus.setOnClickListener(v -> {
+            LayoutInflater inflater = LayoutInflater.from(this);
+
+            View newItemView = inflater.inflate(R.layout.list_item, container, false);
+
+            TextView name = newItemView.findViewById(R.id.name);
+            TextView ball = newItemView.findViewById(R.id.circularProgressBar);
+
+            counter++;
+            name.setText("Алгосы");
+            ball.setText("60");
+
+
+            container.addView(newItemView);
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
